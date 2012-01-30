996f994ad41b7888254e8337644cf73b46de4b88
Sam Reeves <reeves.sam@gmail.com>
2012-01-30 17:41:43 -0500

    hw02 done

---
+++ day2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#---
+#a first program
+#---
+
+#hell fuckin yeah print bitch print
+print "hellloooooooooooooooo, nurse!"
+print "Isn't this a wonderful day????"
+
+#CAUTION MATH IS HAPPENING
+
+print "1 + 3 =",1+3
+print "2 + 8 =",2+8
+print "that was uber-exciting."
